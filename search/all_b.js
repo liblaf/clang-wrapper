9d3cb61133ffe285811fa61b5062258e9ed9317f
liblaf <liblaf@outlook.com>
2022-10-04 11:34:07 +0000

    Deploying to gh-pages from @ liblaf/clang-wrapper@8e97c73586bc7c25598e8ea27f9236b5668ae4f3 🚀

--- search/all_b.js
+++ search/all_b.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['main_0',['main',['../main_8cc.html#a3c04138a5bfe5d72780bb7e82a18e627',1,'main.cc']]],
-  ['main_2ecc_1',['main.cc',['../main_8cc.html',1,'']]],
-  ['main_2eh_2',['main.h',['../main_8h.html',1,'']]]
+  ['main_2ecc_1',['main.cc',['../main_8cc.html',1,'']]]
 ];
--- search/files_4.js
+++ search/files_4.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['main_2ecc_0',['main.cc',['../main_8cc.html',1,'']]],
-  ['main_2eh_1',['main.h',['../main_8h.html',1,'']]]
+  ['main_2ecc_0',['main.cc',['../main_8cc.html',1,'']]]
 ];
