d71c6ec396ff66953eebad8932e8c6590ce88315
Qin Li <30631553+liblaf@users.noreply.github.com>
2022-10-26 22:18:53 +0800

    fix(common): seperate log into multiple files

--- src/common/logging.cc
+++ src/common/logging.cc
@@ -1,19 +1,34 @@
 #include "logging.h"
 
 #include <chrono>
-#include <cstdlib>
 #include <ctime>
 #include <iomanip>
 #include <sstream>
 #include <string>
 #include <system_error>
 
+#include "env.h"
+#include "llvm/ADT/StringRef.h"
 #include "llvm/Support/FileSystem.h"
 #include "llvm/Support/raw_ostream.h"
+#include "path.h"
 
 using namespace llvm;
 
 static std::unique_ptr<Logger> _logger = nullptr;
+
+Logger::Logger(StringRef filename, std::error_code& ec,
+               sys::fs::OpenFlags flags)
+    : raw_fd_ostream(filename, ec, flags), _filename(filename.str()) {}
+
+Logger::~Logger() {
+  this->close();
+  if (fs::exists(this->filename()) && (fs::file_size(this->filename()) < 64)) {
+    fs::remove(this->filename());
+  }
+}
+
+fs::path Logger::filename() const { return this->_filename; }
 
 int Logger::log_level() const { return this->_log_level; }
 
@@ -30,18 +45,29 @@ void Logger::set_log_level(const int new_log_level) {
   }
 }
 
-const char* Logger::get_log_path() {
-  const char* log_path = std::getenv("LOG_PATH");
-  if (log_path) {
-    return log_path;
-  } else {
-    return LOG_PATH;
+fs::path Logger::get_log_path() {
+  std::string log_path = get_env("LOG_PATH", LOG_PATH);
+  auto pos = log_path.find('*');
+  if (pos != std::string::npos) {
+    log_path.replace(pos, 1, Logger::time());
   }
+  return log_path;
 }
 
 Logger& Logger::log(const char* fmt) {
   (*this) << fmt;
   return *this;
+}
+
+std::string Logger::time() {
+  std::ostringstream oss;
+  auto now = std::chrono::high_resolution_clock::now();
+  auto t = std::chrono::high_resolution_clock::to_time_t(now);
+  auto seconds = std::chrono::time_point_cast<std::chrono::seconds>(now);
+  std::chrono::nanoseconds nanoseconds = now - seconds;
+  oss << std::put_time(std::localtime(&t), "%F %T") << "." << std::setw(9)
+      << std::setfill('0') << nanoseconds.count();
+  return oss.str();
 }
 
 Logger& Logger::change_color(raw_ostream::Colors color, bool bold,
@@ -65,9 +91,8 @@ std::string Logger::get_prompt(const std::string& type) {
   auto t = std::chrono::high_resolution_clock::to_time_t(now);
   auto seconds = std::chrono::time_point_cast<std::chrono::seconds>(now);
   std::chrono::nanoseconds nanoseconds = now - seconds;
-  oss << '[' << std::put_time(std::localtime(&t), "%F %T") << "."
-      << std::setw(9) << std::setfill('0') << nanoseconds.count() << "] "
-      << std::setw(10) << std::setfill(' ') << ('[' + type + ']') << ' ';
+  oss << '[' << Logger::time() << "] " << std::setw(10) << std::setfill(' ')
+      << ('[' + type + ']') << ' ';
   return oss.str();
 }
 
@@ -76,8 +101,8 @@ Logger& logger() { return *_logger; }
 static void __attribute__((constructor)) _logger_init() {
   if (_logger) return;
   std::error_code ec;
-  _logger = std::make_unique<Logger>(Logger::get_log_path(), ec,
-                                     sys::fs::OpenFlags::OF_Append);
+  _logger = std::make_unique<Logger>(Logger::get_log_path().string(), ec,
+                                     sys::fs::OpenFlags::OF_None);
   logger().set_log_level();
 }
 
